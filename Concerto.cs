e3e50a6df24558f7be3c578687c675db46c8b36f
Stefano Calzoni <stefanoclz@hotmail.it>
2022-07-06 17:18:40 +0200

    descritte le classe degli eventi

---
+++ Concerto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Concerto : Evento
+    {
+        public string titolo { get; set; }
+        public DateTime data { get; set; }
+        public int capienzaMassima { get; private set; }
+        public int postiPrenotati { get; private set;  }
+
+        public Concerto(string titolo, DateTime data, int postiDisponibili)
+        {
+            if(titolo.Length > 0)
+            {
+                this.titolo = titolo;
+            }
+            DateTime todayFull = DateTime.Now;
+
+            if(data > todayFull)
+            {
+                this.data = data;
+            }
+
+            if (postiDisponibili > 0)
+            {
+                this.capienzaMassima = postiDisponibili;
+            }
+
+            this.postiPrenotati = 0;
+        }
+
+        public override void PrenotaPosti(int param)
+        {
+
+            int postiDisp = this.capienzaMassima - this.postiPrenotati;
+
+            if(param > 0 && param < postiDisp)
+            {
+                this.postiPrenotati += param;
+            }
+
+            Console.WriteLine($"Prenotazione effettuata! Hai prenotato {param} posti, Capacità massima {this.capienzaMassima}, Posti prenotati {this.postiPrenotati}");
+        }
+
+        public override void DisdiciPosti(int param)
+        {
+
+            int postiDisp = this.capienzaMassima - this.postiPrenotati;
+
+            if (param > 0 && param <= postiDisp)
+            {
+                this.postiPrenotati -= param;
+            }
+
+            Console.WriteLine($"Disdetta effettuata! Hai disdetto {param} posti, Capacità massima {this.capienzaMassima}, Posti prenotati {this.postiPrenotati}");
+        }
+
+
+        public override string ToString()
+        {
+           this.data = data.ToString()
+        }
+    }
+}
---
+++ Evento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    abstract class Evento
+    {
+        public string titolo { get; set; }
+
+        public DateTime data { get; set; }
+
+        public int capienzaMassina { get; }
+
+        public int postiPrenotati { get; }
+        public abstract void PrenotaPosti(int param);
+        public abstract void DisdiciPosti(int param);
+
+    }
+}
